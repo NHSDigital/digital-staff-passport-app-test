f00d8dbb793817abf3a588c30db81930b0629315
Tushar Dandwate <114984998+tdandwate1992@users.noreply.github.com>
2025-02-19 08:15:02 +0000

    review and confirm initial creds

--- ios/BDD_IOS/features/steps/dsp_review_and_confirm_initial_credentials_steps.py
+++ ios/BDD_IOS/features/steps/dsp_review_and_confirm_initial_credentials_steps.py
@@ -1,61 +1,61 @@
 """steps for review and confirm initial credentials"""
 from behave import then, when
-from pages.review_and_confirm_initial_credentials_page import ReviewAndConfirmInitialCredentialsPage
+from pages.ios_pages.review_and_confirm_initial_credentials_page import ReviewAndConfirmInitialCredentialsPage
 
 
 @then("user verifies the page heading")
 def user_verifies_page_heading(context):
     """step to call read page heading and assert the expected value"""
     context.review_creds = ReviewAndConfirmInitialCredentialsPage(context.driver)
-    assert "Page heading" in context.review_creds.read_identity_page_heading()
+    assert "Page heading" in context.review_creds.read_identity_page_heading(), "Page heading is not displayed"
 
 
 @then("user verifies question mark icon is present")
 def user_verifies_question_mark_icon(context):
     """step to verify question mark icon is present"""
-    assert context.review_creds.verify_question_mark_icon()
+    assert context.review_creds.verify_question_mark_icon(), "Question mark icon is not displayed"
 
 
 @then("user verifies user account icon is present")
 def user_verifies_user_account_icon(context):
     """step to verify user account icon is present"""
-    assert context.review_creds.verify_user_account_icon()
+    assert context.review_creds.verify_user_account_icon(), "User account icon is not displayed"
 
 
 @then("user verifies Home button is displayed")
 def user_verifies_home_button(context):
     """step to verify Home button is displayed"""
-    assert context.review_creds.verify_home_button()
+    assert context.review_creds.verify_home_button(), "Home button is not displayed"
 
 
 @then("user verifies credentials button is displayed")
 def user_verifies_credentials_button(context):
     """step to verify credentials button is displayed"""
-    assert context.review_creds.verify_credentials_button()
+    assert context.review_creds.verify_credentials_button(), "Credentials button is not displayed"
 
 
 @then("user verifies credentials to confirm has identity credential listed with action required")
 def user_verifies_identity_credential(context):
     """step to verify identity credential is listed with action required"""
-    assert context.review_creds.verify_identity_credential()
+    assert context.review_creds.verify_identity_credential(), "Identity credential is not displayed"
 
 
 @then("user verifies credentials to confirm has right to work credential listed with action required")
 def user_verifies_right_to_work_credential(context):
     """step to verify right to work credential is listed with action required"""
-    assert context.review_creds.verify_right_to_work_credential()
+    assert context.review_creds.verify_right_to_work_credential(), "Right to work credential is not displayed"
 
 
 @then("user verifies credentials to confirm has DBS supporting documents listed with action required")
 def user_verifies_dbs_supporting_documents(context):
     """step to verify DBS supporting documents is listed with action required"""
-    assert context.review_creds.verify_dbs_supporting_documents()
+    assert context.review_creds.verify_dbs_supporting_documents(), "DBS supporting documents is not displayed"
 
 
 @then('user verifies confirmed credentials has "No credentials yet"')
 def user_verifies_no_credentials_yet(context):
     """step to verify no credentials yet message is displayed"""
-    assert context.review_creds.verify_no_credentials_yet()
+    assert context.review_creds.verify_no_credentials_yet(), "No credentials yet message is not displayed"
 
 
 @when("user click identity credential")
@@ -67,7 +67,7 @@ def user_click_identity_credential(context):
 @then("user verifies provided by Trust")
 def user_verifies_provided_by_trust(context):
     """step to verify provided by Trust"""
-    assert context.review_creds.verify_provided_by_trust()
+    assert context.review_creds.verify_provided_by_trust(), "Provided by Trust is not displayed"
 
 
 @then("user verifies the identity credential details - Name,DOB, Legal gender, Nationality, issued on")
@@ -79,7 +79,7 @@ def user_verifies_identity_credential_details(context):
 @then("user verifies link something is not right")
 def user_verifies_something_not_right_link(context):
     """step to verify something is not right link"""
-    assert context.review_creds.verify_something_not_right_link()
+    assert context.review_creds.verify_something_not_right_link(), "Something is not right link is not displayed"
 
 
 @when("user clicks confirm credential button")
@@ -91,19 +91,19 @@ def user_click_confirm_credential_button(context):
 @then("user see Confirming credential spinner")
 def user_see_confirming_credential_spinner(context):
     """step to verify confirming credential spinner"""
-    assert context.review_creds.verify_confirming_credential_spinner()
+    assert context.review_creds.verify_confirming_credential_spinner(), "Confirming credential spinner is not displayed"
 
 
 @then("user lands on credentials confirmed page")
 def user_lands_on_credentials_confirmed_page(context):
     """step to verify user lands on credentials confirmed page"""
-    assert context.review_creds.verify_credentials_confirmed_page()
+    assert context.review_creds.verify_credentials_confirmed_page(), "User is not on credentials confirmed page"
 
 
 @then("user verifies the text on the page")
 def user_verifies_text_on_page(context):
     """step to verify the text on the page"""
-    assert context.review_creds.verify_text_on_page()
+    assert context.review_creds.verify_text_on_page(), "Text is not displayed on the page"
 
 
 @when("user clicks on Back to my credentials button")
@@ -115,19 +115,19 @@ def user_click_back_to_my_credentials_button(context):
 @then("user lands on credentials page")
 def user_lands_on_credentials_page(context):
     """step to verify user lands on credentials page"""
-    assert context.review_creds.verify_credentials_page()
+    assert context.review_creds.verify_credentials_page(), "User is not on credentials page"
 
 
 @then("user verifies identity credential is removed from credentials to confirm")
 def user_verifies_identity_credential_removed(context):
     """step to verify identity credential is removed from credentials to confirm"""
-    assert context.review_creds.verify_identity_credential_removed()
+    assert not context.review_creds.verify_identity_credential_removed(), "Identity credential is not removed"
 
 
 @then("user verifies identity is listed under confirmed credentials")
 def user_verifies_identity_listed(context):
     """step to verify identity is listed under confirmed credentials"""
-    assert context.review_creds.verify_identity_listed()
+    assert context.review_creds.verify_identity_listed(), "Identity is not listed under confirmed credentials"
 
 
 @when("user click right to work credential")
@@ -140,13 +140,13 @@ def user_click_right_to_work_credential(context):
     "user verifies the right to work credential details - Photo of your face, name, DOB, Biometric page, passport expiry date")
 def user_verifies_right_to_work_credential_details(context):
     """step to verify right to work credential details"""
-    assert context.review_creds.verify_right_to_work_credential_details()
+    assert context.review_creds.verify_right_to_work_credential_details(), "Right to work credential details are not displayed"
 
 
 @then("user verifies link something went wrong")
 def user_verifies_something_went_wrong_link(context):
     """step to verify something went wrong link"""
-    assert context.review_creds.verify_something_went_wrong_link()
+    assert context.review_creds.verify_something_went_wrong_link(), "Something went wrong link is not displayed"
 
 
 @then("use click confirm credential button")
@@ -158,18 +158,18 @@ def user_click_confirm_credential_button(context):
 @then("user see a spinner page with confirming credential")
 def user_see_spinner_page_with_confirming_credential(context):
     """step to verify spinner page with confirming credential"""
-    assert context.review_creds.verify_spinner_page_with_confirming_credential()
+    assert context.review_creds.verify_spinner_page_with_confirming_credential(), "Spinner page is not displayed"
 
 
 @then("user verifies right to work credential is removed from credentials to confirm")
 def user_verifies_right_to_work_credential_removed(context):
     """step to verify right to work credential is removed from credentials to confirm"""
-    assert context.review_creds.verify_right_to_work_credential_removed()
+    assert not context.review_creds.verify_right_to_work_credential_removed(), "Right to work credential is not removed"
 
 @then("user verifies right to work is listed under confirmed credentials")
 def user_verifies_right_to_work_listed(context):
     """step to verify right to work is listed under confirmed credentials"""
-    assert context.review_creds.verify_right_to_work_listed()
+    assert context.review_creds.verify_right_to_work_listed(), "Right to work is not listed under confirmed credentials"
 
 
 @when("user click DBS supporting documents")
@@ -182,7 +182,7 @@ def user_click_dbs_supporting_documents(context):
     "user verifies the DBS supporting documents details - Name, Date of birth, Verified current address, Resident from, Passport number, Passport nationality,Passport issue date, Confirm credential button")
 def user_verifies_dbs_supporting_documents_details(context):
     """step to verify DBS supporting documents details"""
-    assert context.review_creds.verify_dbs_supporting_documents_details()
+    assert context.review_creds.verify_dbs_supporting_documents_details(), "DBS supporting documents details are not displayed"
 
 
 @then("user click confirm credential button")
@@ -194,10 +194,10 @@ def user_click_confirm_credential_button(context):
 @then("user verifies DBS supporting documents is removed from credentials to confirm")
 def user_verifies_dbs_supporting_documents_removed(context):
     """step to verify DBS supporting documents is removed from credentials to confirm"""
-    assert context.review_creds.verify_dbs_supporting_documents_removed()
+    assert not context.review_creds.verify_dbs_supporting_documents_removed(), "DBS supporting documents is not removed"
 
 
 @then("user verifies DBS supporting documents is listed under confirmed credentials")
 def user_verifies_dbs_supporting_documents_listed(context):
     """step to verify DBS supporting documents is listed under confirmed credentials"""
-    assert context.review_creds.verify_dbs_supporting_documents_listed()
+    assert context.review_creds.verify_dbs_supporting_documents_listed(), "DBS supporting documents is not listed under confirmed credentials"
---
+++ ios/BDD_IOS/pages/ios_pages/review_and_confirm_initial_credentials_page.py
@@ -0,0 +1,209 @@
+"""this module to be used to call in the steps
+    definition for the review initial 3 creds page
+"""
+from pages.base_page import BasePage
+
+
+class ReviewAndConfirmInitialCredentialsPage(BasePage):
+    """credentials page with dates locators"""
+    credentials_page_heading = ""
+    credentials_to_confirm = ""
+    identity_credentials = ""
+    right_to_work_credentials = ""
+    dbs_supporting_information = ""
+    action_required = ""
+    confirmed_credentials = ""
+    no_credentials_yet = ""
+    home_icon = ""
+    credentials_icon = ""
+    question_icon = ''
+    account_icon = ''
+    """identity credentials page locators"""
+    identity_page_heading = ""
+    provided_by_trust = ""
+    photo_of_your_face = ''
+    name_title = ''
+    name_value = ''
+    dob_title = ''
+    dob_value = ''
+    legal_gender_title = ''
+    legal_gender_value = ''
+    nationality_title = ''
+    nationality_value = ''
+    origin_title = ''
+    origin_value = ''
+    issued_on_title = ''
+    issued_on_value = ''
+    something_is_not_right_link = ''
+    confirm_credential_button = ''
+    """confirming credential1 page locators"""
+    confirming_credentials_spinner = ''
+    """credentials confirmed page locators"""
+    credential_confirmed_title = ''
+    credentials_confirmed_message = ''
+    back_to_my_credentials_button = ''
+    """right to work page locators"""
+    right_to_work_heading = ''
+    rtw_name_title = ''
+    rtw_name_value = ''
+    rtw_dob_title = ''
+    rtw_dob_value = ''
+    rtw_biometric_page_title = ''
+    rtw_passport_expiry_date_title = ''
+    rtw_something_is_not_right_link = ''
+    rtw_confirm_credential_button = ''
+    """dbs supporting information"""
+    dbs_supporting_info_heading = ''
+    dbs_name_title = ''
+    dbs_name_value = ''
+    dbs_dob_title = ''
+    dbs_dob_value = ''
+    dbs_verified_current_address_title = ''
+    dbs_verified_current_address_value = ''
+    dbs_resident_form_title = ''
+    dbs_resident_from_value = ''
+    dbs_passport_number_title = ''
+    dbs_passport_number_value = ''
+    dbs_passport_nationality_title = ''
+    dbs_passport_nationality_value = ''
+    dbs_passport_issue_date_title = ''
+    dbs_passport_issue_date_value = ''
+    dbs_some_thing_is_not_right_link = ''
+    dbs_confirm_credential_button = ''
+
+    def read_identity_page_heading(self):
+        """method to read the page heading"""
+        return self.read_value_from_element(self.identity_page_heading)
+
+    def verify_question_mark_icon(self):
+        """method to verify the question mark icon"""
+        return self.verify_element_displayed(self.question_icon)
+
+    def verify_user_account_icon(self):
+        """method to verify the account icon"""
+        return self.verify_element_displayed(self.account_icon)
+
+    def verify_home_button(self):
+        """method to verify the home button"""
+        return self.verify_element_displayed(self.home_icon)
+
+    def verify_credentials_button(self):
+        """method to verify the credentials icon"""
+        return self.verify_element_displayed(self.credentials_icon)
+
+
+    def verify_identity_credential(self):
+        """method to verify the identity credential"""
+        return self.verify_element_displayed(self.identity_credentials)
+
+    def verify_right_to_work_credentials(self):
+        """method to verify the right to work credentials"""
+        return self.verify_element_displayed(self.right_to_work_credentials)
+
+    def verify_dbs_supporting_documents(self):
+        """method to verify the dbs supporting documents"""
+        return self.verify_element_displayed(self.dbs_supporting_information)
+
+    def verify_no_credentials_yet(self):
+        """method to verify the no credentials yet message"""
+        return self.verify_element_displayed(self.no_credentials_yet)
+
+    def click_identity_credential(self):
+        """method to click on identity credential"""
+        self.click_element(self.identity_credentials)
+
+
+    def verify_provided_by_trust(self):
+        """method to verify provided by trust"""
+        return self.verify_element_displayed(self.provided_by_trust)
+
+    def verify_identity_credential_details(self):
+        """method to verify the identity credential details"""
+        return self.verify_element_displayed(self.photo_of_your_face) and self.verify_element_displayed(self.name_title) and self.verify_element_displayed(self.name_value) and self.verify_element_displayed(self.dob_title) and self.verify_element_displayed(self.dob_value) and self.verify_element_displayed(self.legal_gender_title)
+
+
+    def verify_something_not_right_link(self):
+        """method to verify the something not right link"""
+        return self.verify_element_displayed(self.something_is_not_right_link)
+
+    def click_confirm_credential_button(self):
+        """method to click on confirm credential button"""
+        self.click_element(self.confirm_credential_button)
+
+
+    def verify_confirming_credential_spinner(self):
+        """method to verify the confirming credential spinner"""
+        return self.verify_element_displayed(self.confirming_credentials_spinner)
+
+
+    def verify_credentials_confirmed_page(self):
+        """method to verify the credentials confirmed page"""
+        return self.verify_element_displayed(self.credential_confirmed_title)
+
+    def verify_text_on_page(self):
+        """method to verify the text on the page"""
+        return self.verify_element_displayed(self.credentials_confirmed_message)
+
+
+    def click_back_to_my_credentials_button(self):
+        """method to click back to my credentials button"""
+        self.click_element(self.back_to_my_credentials_button)
+
+    def verify_credentials_page(self):
+        """method to verify the credentials page"""
+        return self.verify_element_displayed(self.credentials_page_heading)
+
+    def verify_identity_credential_removed(self):
+        """method to verify the identity credential removed"""
+        return self.verify_element_displayed(self.identity_credentials)
+
+    def verify_identity_listed(self):
+        """method to verify the identity listed"""
+        return self.verify_element_displayed(self.identity_credentials)
+
+
+    def click_right_to_work_credentials(self):
+        """method to click on right to work credentials"""
+        self.click_element(self.right_to_work_credentials)
+
+    def verify_right_to_work_credential_details(self):
+        """method to verify the right to work credential details"""
+        return self.verify_element_displayed(self.rtw_name_title) and self.verify_element_displayed(self.rtw_name_value) and self.verify_element_displayed(self.rtw_dob_title) and self.verify_element_displayed(self.rtw_dob_value) and self.verify_element_displayed(self.rtw_biometric_page_title) and self.verify_element_displayed(self.rtw_passport_expiry_date_title)
+
+
+    def verify_something_went_wrong_link(self):
+        """method to verify the something went wrong link"""
+        return self.verify_element_displayed(self.rtw_something_is_not_right_link)
+
+    def verify_spinner_page_with_confirming_credential(self):
+        """method to verify the spinner page with confirming credential"""
+        return self.verify_element_displayed(self.confirming_credentials_spinner)
+
+    def verify_right_to_work_credential_removed(self):
+        """method to verify the right to work credential removed"""
+        return self.verify_element_displayed(self.right_to_work_credentials)
+
+    def verify_right_to_work_listed(self):
+        """method to verify the right to work listed"""
+        return self.verify_element_displayed(self.right_to_work_credentials)
+
+    def click_dbs_supporting_documents(self):
+        """method to click on dbs supporting documents"""
+        self.click_element(self.dbs_supporting_information)
+
+    def verify_dbs_supporting_documents_details(self):
+        """method to verify the dbs supporting documents details"""
+        return self.verify_element_displayed(self.dbs_name_title) and self.verify_element_displayed(self.dbs_name_value) and self.verify_element_displayed(self.dbs_dob_title) and self.verify_element_displayed(self.dbs_dob_value) and self.verify_element_displayed(self.dbs_verified_current_address_title) and self.verify_element_displayed(self.dbs_verified_current_address_value) and self.verify_element_displayed(self.dbs_resident_form_title) and self.verify_element_displayed(self.dbs_resident_from_value) and self.verify_element_displayed(self.dbs_passport_number_title) and self.verify_element_displayed(self.dbs_passport_number_value) and self.verify_element_displayed(self.dbs_pass)
+
+
+    def verify_dbs_supporting_documents_removed(self):
+        """method to verify the dbs supporting documents removed"""
+        return self.verify_element_displayed(self.dbs_supporting_information)
+
+    def verify_dbs_supporting_documents_listed(self):
+        """method to verify the dbs supporting documents listed"""
+        return self.verify_element_displayed(self.dbs_supporting_information)
+
+
+
+
